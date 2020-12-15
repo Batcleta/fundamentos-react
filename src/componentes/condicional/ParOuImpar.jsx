a178c7f92aa3aff77a7eacec37114b6a9103edff
Washington Ferreira <thewazig.wf@gmail.com>
2020-12-15 14:20:42 -0300

    maps integrado a uma função

--- src/componentes/condicional/ParOuImpar.jsx
+++ src/componentes/condicional/ParOuImpar.jsx
@@ -10,9 +10,21 @@ export default props => {
         )
     })
 
+
+    const contImparPar = _ => {
+        return Numeros.map((num, i) => {
+            return (
+                <div key={i}> {num % 2 === 0 ? <div>Par</div> : <div>imPar</div>} </div>
+            )
+        })
+    }
+
     return (
         <div>
+            <div>operação com maps em variavel</div>
             {contParImpar}
+            <div>Operação com maps em função</div>
+            {contImparPar()}
         </div>
     )
 }
--- src/data/numeração.js
+++ src/data/numeração.js
@@ -1,3 +1,3 @@
-const parouimpar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const parouimpar = [1, 2, 3, 4, 5, 6]
 
 export default parouimpar
